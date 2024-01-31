e9139c1142ccd97e95a1b0d1227da31c45f4a509
monpasye <dlafanre@gmail.com>
2024-01-31 22:39:42 +0600

    Logo and Top Navigation fixed

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import TopNavigation from './topnavigation.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <div style={{float: 'left', marginLeft: '200px', marginTop: '50px'}}><Logo /></div>
-    <TopNavigation/>
+    <div style={{float: 'left', marginTop: '60px', marginLeft: "200px"}}><Logo /></div>
+    <div style={{float: 'right', marginRight: '200px', marginTop: '60px'}}><TopNavigation /></div>
   </React.StrictMode>,
 )
